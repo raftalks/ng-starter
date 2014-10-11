011aef0a0f3daad93183ec23b905769fcfee4af3
Raf <7909000@gmail.com>
2014-10-12 02:13:05 +0500

    add app config provider to bootstrap the app

--- src/app/app.js
+++ src/app/app.js
@@ -5,15 +5,40 @@
 	/* global angular */
 	angular.module('app', [
 		'app.common',
-        'app.home',
-		'app.dashboard'
+        'app.frontend',
+		'app.backend'
 	])
 
-	.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
-        function($stateProvider, $urlRouterProvider, $locationProvider) {
+	.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 'appConfigProvider',
+        function($stateProvider, $urlRouterProvider, $locationProvider, appConfigProvider) {
 
-        	$locationProvider.html5Mode(true);
+            /*
+            Application Configuration Provider is used to set config keys used throughout the application.
+            This works as a global memory space for the application on runtime.
+            Feel free to set your own config values here.
+            */
+            appConfigProvider.set('Server', 'http://localhost');    // API SERVER FOR REST CALLS
+            appConfigProvider.set('RegistryKey', 'Registry');       // REGISTRY KEY IDENTIFIER
+            appConfigProvider.set('AuthSessionId', 'AuthSession');  // AUTH SESSION KEY IDENTIFIER
+            appConfigProvider.set('SessionID', 'AppSession');       // APP SESSION KEY IDENTIFIER
 
+            // Any custom Configurations that you like to add here should follow under this line.
+
+            // End of custom configs.
+
+
+            /*
+            Enabling this feature will remove the hash tag from the URL to make it work in modern browsers. However
+            this is still on experimental stage and is not recommended for production yet. Enabling this may also require
+            setting some sort of URL rewriting when the same URL is requested directly on the browser.
+            */
+        	$locationProvider.html5Mode(false);
+
+
+            /*
+            Here, we use stateProvider to set the base state of the app which is an abstract state that other states will extend from.
+            Do not modify this unless you know what you are doing here.
+            */
             $stateProvider.state('app', {
                 abstract: true,
                 views: {
--- src/app/readme.txt
+++ src/app/readme.txt
@@ -3,3 +3,7 @@ APP DIRECTORY
 
 
 This is the directory to add the application layer files and it's views.
+
+This frontend folder contains every module and its views related to the frontend of the system or website.
+
+The backend folder contains every thing related to the application backend where administrators or supervisors will use the application to process and configure the application.
